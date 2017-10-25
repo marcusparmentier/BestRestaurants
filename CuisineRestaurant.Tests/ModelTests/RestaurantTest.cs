70f034472ff5238108c6ef3c88b3738b022c5738
Hansen Hansen and Marcus Parmentier <marcus@>
2017-10-25 14:07:28 -0700

    Add tests to the save and add the find method and test. Restaurant is funtional.

--- CuisineRestaurant.Tests/ModelTests/RestaurantTest.cs
+++ CuisineRestaurant.Tests/ModelTests/RestaurantTest.cs
@@ -6,11 +6,16 @@ using CuisineRestaurant.Models;
 namespace CuisineRestaurant.Test
 {
   [TestClass]
-  public class RestaurantTests
+  public class RestaurantTests : IDisposable
   {
     public RestaurantTests()
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=cuisine_restaurant_test;";
+    }
+    public void Dispose()
+    {
+      Restaurant.DeleteAll();
+
     }
 
     [TestMethod]
@@ -24,7 +29,7 @@ namespace CuisineRestaurant.Test
     }
 
     [TestMethod]
-    public void Equals_OverRideTrueForSameDescription_Restaurant()
+    public void Equals_OverRideTrueForSameName_Restaurant()
     {
       //Arrange, Act
       Restaurant firstRestaurant = new Restaurant("Wendy's", 1);
@@ -33,5 +38,62 @@ namespace CuisineRestaurant.Test
       //Assert
       Assert.AreEqual(firstRestaurant, secondRestaurant);
     }
+
+    [TestMethod]
+    public void Save_SavesRestaurantToDatabase_RestaurantList()
+    {
+      //Arrange
+      Restaurant testRestaurant = new Restaurant("KFC", 100);
+      testRestaurant.Save();
+
+      //Act
+      // string resultString = "";
+      // int resultId = 0;
+      // int Cuisine = 0;
+      List<Restaurant> result = Restaurant.GetAll();
+      // foreach(var list in result)
+      // {
+      //   resultString = list.GetName();
+      //   resultId = list.GetId();
+      //   Cuisine = list.GetCuisineId();
+      // }
+      // Console.WriteLine(resultString + " " + resultId + " " + Cuisine);
+      List<Restaurant> testList = new List<Restaurant>{testRestaurant};
+
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
+
+    [TestMethod]
+    public void Save_DatabaseAssignsIdToObject_Id()
+    {
+      //Arrange
+      Restaurant testRestaurant = new Restaurant("KFC", 1);
+      testRestaurant.Save();
+
+      //Act
+      Restaurant savedRestaurant = Restaurant.GetAll()[0];
+
+      int result = savedRestaurant.GetId();
+      int testId = testRestaurant.GetId();
+
+      //Assert
+      Assert.AreEqual(testId, result);
+    }
+
+    [TestMethod]
+    public void Find_FindsRestaurantInDatabase_Restaurant()
+    {
+      //Arrange
+      Restaurant testRestaurant = new Restaurant("KFC", 1);
+      testRestaurant.Save();
+
+      //Act
+      Restaurant foundRestaurant = Restaurant.Find(testRestaurant.GetId());
+
+      //Assert
+      Assert.AreEqual(testRestaurant, foundRestaurant);
+    }
+
   }
 }
--- CuisineRestaurant/Models/Restaurant.cs
+++ CuisineRestaurant/Models/Restaurant.cs
@@ -18,40 +18,41 @@ namespace CuisineRestaurant.Models
     }
 
     public override bool Equals(System.Object otherRestaurant)
-   {
-     if (!(otherRestaurant is Restaurant))
-     {
-       return false;
-     }
-     else
-     {
-       Restaurant newRestaurant = (Restaurant) otherRestaurant;
-       bool idEquality = this.GetId() == newRestaurant.GetId();
-       bool nameEquality = this.GetName() == newRestaurant.GetName();
-       bool cuisineEquality = this.GetCuisineId() == newRestaurant.GetCuisineId();
-       return (idEquality && nameEquality && cuisineEquality);
-     }
-   }
+    {
+      if (!(otherRestaurant is Restaurant))
+      {
+        return false;
+      }
+      else
+      {
+        Restaurant newRestaurant = (Restaurant) otherRestaurant;
+        bool idEquality = this.GetId() == newRestaurant.GetId();
+        bool nameEquality = this.GetName() == newRestaurant.GetName();
+        bool cuisineEquality = this.GetCuisineId() == newRestaurant.GetCuisineId();
+        return (idEquality && nameEquality && cuisineEquality);
+      }
+    }
 
-   public override int GetHashCode()
-   {
-     return this.GetName().GetHashCode();
-   }
+    public override int GetHashCode()
+    {
+      return this.GetName().GetHashCode();
+    }
 
-   public int GetId()
-   {
-     return _id;
-   }
+    public int GetId()
+    {
+      return _id;
+    }
 
-   public string GetName()
-   {
-     return _name;
-   }
+    public string GetName()
+    {
+      return _name;
+    }
 
-   public int GetCuisineId()
-   {
-     return _cuisineId;
-   }
+    public int GetCuisineId()
+    {
+      return _cuisineId;
+    }
+
 
 
     public static List<Restaurant> GetAll()
@@ -67,7 +68,7 @@ namespace CuisineRestaurant.Models
         string restaurantName = rdr.GetString(0);
         int id = rdr.GetInt32(1);
         int cuisineId = rdr.GetInt32(2);
-        Restaurant newRestaurant = new Restaurant(restaurantName, id, cuisineId);
+        Restaurant newRestaurant = new Restaurant(restaurantName, cuisineId, id);
         allRestaurant.Add(newRestaurant);
       }
       conn.Close();
@@ -78,35 +79,79 @@ namespace CuisineRestaurant.Models
       return allRestaurant;
     }
 
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO restaurant (name, cuisineId) VALUES (@name, @cuisineId);";
+
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@name";
+      name.Value = this._name;
+      cmd.Parameters.Add(name);
+
+      MySqlParameter cuisineId = new MySqlParameter();
+      cuisineId.ParameterName = "@cuisineId";
+      cuisineId.Value = this._cuisineId;
+      cmd.Parameters.Add(cuisineId);
 
 
-    // public void Save()
-    // {
-    //   MySqlConnection conn = DB.Connection();
-    //   conn.Open();
-    //
-    //   var cmd = conn.CreateCommand() as MySqlCommand;
-    //   cmd.CommandText = @"INSERT INTO restaurants (name, cuisineId) VALUES (@name, @cuisineId);";
-    //
-    //   MySqlParameter name = new MySqlParameter();
-    //   name.ParameterName = "@name";
-    //   name.Value = this._name;
-    //   cmd.Parameters.Add(name);
-    //
-    //   MySqlParameter cuisineId = new MySqlParameter();
-    //   cuisineId.ParameterName = "@cuisineId";
-    //   cuisineId.Value = this._cuisineId;
-    //   cmd.Parameters.Add(cuisineId);
-    //
-    //
-    //   cmd.ExecuteNonQuery();
-    //   _id = (int) cmd.LastInsertedId;
-    //   conn.Close();
-    //   if (conn != null)
-    //   {
-    //     conn.Dispose();
-    //   }
-    //
-    // }
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+
+    }
+
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM restaurant;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static Restaurant Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM restaurant WHERE id = (@searchId);";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = id;
+      cmd.Parameters.Add(searchId);
+
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int restaurantId = 0;
+      string restaurantName = "";
+      int restaurantCuisineId = 0;
+
+      while(rdr.Read())
+      {
+        restaurantName = rdr.GetString(0);
+        restaurantId = rdr.GetInt32(1);
+        restaurantCuisineId = rdr.GetInt32(2);
+      }
+      Restaurant newRestaurant = new Restaurant(restaurantName, restaurantCuisineId, restaurantId);
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return newRestaurant;
+    }
   }
 }
